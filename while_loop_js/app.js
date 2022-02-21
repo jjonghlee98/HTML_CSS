1090ed80d5b6012eee136d04575a435f260f0197
Jonghwan Lee <jjonghlee98@gmail.com>
2022-02-22 01:13:44 +0900

    next test

--- while_loop_js/app.js
+++ while_loop_js/app.js
@@ -32,7 +32,7 @@
 
 let maximum = parseInt(prompt("Enter the maximum number!"));
 while (!maximum) {
-  maximum = parseInt(prompt("Enter a valid number!"));
+    maximum = parseInt(prompt("Enter a valid number!"));
 }
 
 const targetNum = Math.floor(Math.random() * maximum) + 1;
